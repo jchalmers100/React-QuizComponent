f04ac526c188de7c7b96bf73d02dfd9cb91429f7
james chalmers <james.chalmers@footballpools.com>
2020-03-12 13:53:45 +0000

    Completed module

--- src/QuizEnd.js
+++ src/QuizEnd.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react'
-import QuizQuestion from './QuizQuestion'
 
-class QuizEnd extends React.Component {
-    render() {
-        return(
-            <div>
-                <QuizQuestion/>
-            <p>Thanks for playing!</p>
-            <a href=''>Reset Quiz</a>
-          </div>
-        )
-    }
+class QuizEnd extends Component {
+  render() {
+    return (
+      <div>
+        <p>Thanks for playing!</p>
+        <a href=''>Reset Quiz</a>
+      </div>      
+    )
+  }
 }
 
 export default QuizEnd
